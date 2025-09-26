484a9bb787472e381da14e43cad167c88f2137e6
Ray <166890725+Hashcat07@users.noreply.github.com>
2025-09-26 19:48:48 +0530

    Completed Funtionality except resize

--- hashmap.js
+++ hashmap.js
@@ -1,88 +1,102 @@
-class HashMap{
-    constructor(loadFactor=0.75,capacity=16){
-        this.loadFactor=loadFactor
-        this.capacity=capacity
-        this.hashMap=new Array(capacity)
+import { LinkedList } from "./linkedList.js";
+class HashMap {
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.hashMap = new Array(capacity);
+  }
+  hash(key) {
+    let hashCode = 0;
+    let primeNumber = 31;
+    for (let i = 0; i < key.length; i++)
+      hashCode += (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    return hashCode;
+  }
+  set(key, value) {
+    let index = this.hash(key);
+    if (!this.hashMap[index])
+    {
+        let list=new LinkedList()
+        list.append(key,value)
+        this.hashMap[index]=list
     }
-    hash(key){
-        let hashCode=0
-        let primeNumber=31
-        for(let i=0;i<key.length;i++)
-            hashCode+=(primeNumber*hashCode+key.charCodeAt(i)) % this.capacity
-        return hashCode
+    else {
+    let list=this.hashMap[index]    
+    if(this.has(key))
+        list.updateNode(key,value)
+    else
+        list.append(key,value)
     }
-    set(key,value){
-        let index=this.hash(key)
-        if(!this.hashMap[index])
-            this.hashMap[index]={
-        key:key,
-        value:value
+    
+  }
+  get(key) {
+    let index = this.hash(key);
+    if (index < 0 || index >= this.hashMap.length) {
+      throw new Error("Trying to access index out of bounds");
     }
-    else{
-        if(this.hashMap[index].key===key)
-            this.hashMap[index].value=value
+    if(!this.hashMap[index]) return null; 
+    return this.hashMap[index].find(key) 
+  }
+  has(key) {
+    let index = this.hash(key);
+    if (index < 0 || index >= this.hashMap.length) {
+      throw new Error("Trying to access index out of bounds");
     }
+    if (this.hashMap[index]&&this.hashMap[index].contains(key)) return true;
+    return false;
+  }
+  remove(key) {
+    if (this.has(key)) {
+      this.hashMap[this.hash(key)].removeAt(key);
+      return true;
+    } else return false;
+  }
+  length() {
+    let length = 0;
+    for (let i = 0; i < this.hashMap.length; i++) {
+      if (this.hashMap[i]) length+=this.hashMap[i].size();
     }
-    get(key){
-        let index=this.hash(key)
-        if (index < 0 || index >= this.hashMap.length) {
-  throw new Error("Trying to access index out of bounds");
-}
-    else if(!this.hashMap[index]) return null
-    else return this.hashMap[index].value
-    }
-    has(key){
-let index=this.hash(key)
-        if (index < 0 || index >= this.hashMap.length) {
-  throw new Error("Trying to access index out of bounds");
-}
-    if(this.hashMap[index].key===key)return true
-    return false
-    }
-    remove(key){
-        if(this.has(key)){
-            this.hashMap[this.hash(key)]=undefined
-        return true
+    return length;
+  }
+  clear() {
+    this.hashMap = new Array(this.capacity);
+  }
+  keys() {
+    let keys = [];
+    this.hashMap.forEach((bucket) => {
+      if (bucket) {
+        let tmp=bucket.head()
+        while(tmp!=null){
+            keys.push(tmp.key)
+            tmp=tmp.nextNode
         }
-        else
-            return false
-
-    }
-    length(){
-        let length=0
-        for(let i=0;i<this.hashMap.length;i++)
-        {
-            if(this.hashMap[i])
-                length++
+      };
+    });
+    return keys;
+  }
+  values() {
+    let values = [];
+    this.hashMap.forEach((bucket) => {
+      if (bucket) {
+        let tmp=bucket.head()
+        while(tmp!=null){
+            values.push(tmp.value)
+            tmp=tmp.nextNode
         }
-        return length
-    }
-    clear(){
-        this.hashMap=new Array(this.capacity)
-
-    }
-    keys(){
-        let keys=[]
-        this.hashMap.forEach(bucket=>{
-            if(bucket)
-                keys.push(bucket.key)
-        })
-        return keys
-    }
-    values(){
-        let values=[]
-        this.hashMap.forEach(bucket=>{
-            if(bucket)
-                values.push(bucket.value)
-        })
-        return values
-    }
-    entries(){
-        let entries=[]
-        this.hashMap.forEach(bucket=>{
-            entries.push([bucket.key,bucket.value])
-        })
-        return entries
-    }
-
+      };
+    });
+    return values;
+  }
+  entries() {
+    let entries = [];
+    this.hashMap.forEach((bucket) => {
+        if(bucket){
+            let tmp=bucket.head()
+            while(tmp!=null){
+                entries.push([tmp.key, tmp.value]);
+                tmp=tmp.nextNode}
+            }
+    });
+    return entries;
+  }
 }
---
+++ linkedList.js
@@ -0,0 +1,103 @@
+class LinkedList {
+  constructor() {
+    this.headNode = null;
+    this.tailNode = null;
+  }
+  append(key,value) {
+    let newNode = new node(key,value);
+    if (this.headNode === null) {
+      this.headNode = newNode;
+      this.tailNode = newNode;
+    } else {
+      let tail = this.tail();
+      tail.nextNode = newNode;
+      this.tailNode = newNode;
+    }
+  }
+  updateNode(key,value)
+  {
+    let tmp=this.headNode
+    while(!tmp.key===key)
+      tmp=tmp.nextNode
+    tmp.value=value
+  }
+  size() {
+    let length = 0;
+    let tmp = this.head();
+    while (tmp != null) {
+      length++;
+      tmp = tmp.nextNode;
+    }
+    return length;
+  }
+  head() {
+    return this.headNode;
+  }
+  tail(){
+    return this.tailNode}
+  contains(key) {
+    let tmp = this.head();
+     while(tmp!=null){
+      if(tmp.key===key)
+        return true
+      tmp=tmp.nextNode
+}
+    return false;
+  }
+  at(key) {
+    let tmp = this.head();
+    while(tmp!=null)
+    {
+      if(tmp.key===key)return tmp
+      tmp=tmp.nextNode
+    }
+    return null;
+  }
+  find(key) {
+    let tmp = this.head();
+    let i = 0;
+    while(tmp!=null){
+      if(tmp.key===key)
+        return tmp.value
+      tmp=tmp.nextNode
+    }
+    return "Not Found";
+  }
+  toString() {
+    let tmp = this.head();
+    let list = "";
+    if (this.headNode === null && this.tailNode === null) return "Empty List";
+    while (tmp != null) {
+      list += `${tmp.key}->`;
+      tmp = tmp.nextNode;
+    }
+    return list+`${null}`;
+  }
+  removeAt(key) {
+    if(!this.headNode) return 
+    let tmp = this.head();
+    if(tmp.key===key)
+    {
+      this.headNode=this.headNode.nextNode
+      if(!this.headNode){
+        this.tailNode=null
+        return
+      }
+      return
+    }
+    let atIndex = this.at(key);
+    if(!atIndex) return
+    while (tmp.nextNode != atIndex) tmp = tmp.nextNode;
+    tmp.nextNode = atIndex.nextNode;
+  }
+}
+
+class node {
+  constructor(key,value) {
+    this.value = value;
+    this.key=key
+    this.nextNode = null;
+  }
+}
+
+export { LinkedList };
