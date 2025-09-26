6d3b41e24d32a54c713e8c057cd3aaababf26a2e
Ray <166890725+Hashcat07@users.noreply.github.com>
2025-09-26 18:05:19 +0530

    Finished implementation(Assuming no Collision)

--- hashmap.js
+++ hashmap.js
@@ -1,8 +1,8 @@
 class HashMap{
-    constructor(loadFactor,capacity){
+    constructor(loadFactor=0.75,capacity=16){
         this.loadFactor=loadFactor
         this.capacity=capacity
-        this.hash=new Array(capacity)
+        this.hashMap=new Array(capacity)
     }
     hash(key){
         let hashCode=0
@@ -12,41 +12,77 @@ class HashMap{
         return hashCode
     }
     set(key,value){
-        let hashIndex=this.hash(key)
-        if(!this.hash[hashIndex])
-            this.hash[hashIndex]={
+        let index=this.hash(key)
+        if(!this.hashMap[index])
+            this.hashMap[index]={
         key:key,
         value:value
     }
     else{
-        if(this.hash[hashIndex].key===key)
-            this.hash[hashIndex].value=value
+        if(this.hashMap[index].key===key)
+            this.hashMap[index].value=value
     }
     }
     get(key){
-        let hashIndex=this.hash(key)
-        if(!this.hash[hashIndex]) return null
-        if(this.hash[hashIndex].key===key) return this.hash[hashIndex].value
+        let index=this.hash(key)
+        if (index < 0 || index >= this.hashMap.length) {
+  throw new Error("Trying to access index out of bounds");
+}
+    else if(!this.hashMap[index]) return null
+    else return this.hashMap[index].value
     }
     has(key){
-
+let index=this.hash(key)
+        if (index < 0 || index >= this.hashMap.length) {
+  throw new Error("Trying to access index out of bounds");
+}
+    if(this.hashMap[index].key===key)return true
+    return false
     }
     remove(key){
+        if(this.has(key)){
+            this.hashMap[this.hash(key)]=undefined
+        return true
+        }
+        else
+            return false
 
     }
     length(){
-
+        let length=0
+        for(let i=0;i<this.hashMap.length;i++)
+        {
+            if(this.hashMap[i])
+                length++
+        }
+        return length
     }
     clear(){
+        this.hashMap=new Array(this.capacity)
 
     }
     keys(){
-
+        let keys=[]
+        this.hashMap.forEach(bucket=>{
+            if(bucket)
+                keys.push(bucket.key)
+        })
+        return keys
     }
     values(){
-
+        let values=[]
+        this.hashMap.forEach(bucket=>{
+            if(bucket)
+                values.push(bucket.value)
+        })
+        return values
     }
     entries(){
-
+        let entries=[]
+        this.hashMap.forEach(bucket=>{
+            entries.push([bucket.key,bucket.value])
+        })
+        return entries
     }
+
 }
